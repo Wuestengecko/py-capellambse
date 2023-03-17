5cc8d7b90a9793d7b7484845732434aecdc1df73
Martin Lehmann <martin.lehmann@deutschebahn.com>
2023-03-17 16:49:47 +0100

    feat(filtering): Add support for Filtering result sets [WIP]

--- src/capellambse/extensions/filtering.py
+++ src/capellambse/extensions/filtering.py
@@ -4,6 +4,7 @@
 
 from __future__ import annotations
 
+import abc
 import collections.abc as cabc
 import contextlib
 import logging
@@ -29,10 +30,6 @@ NS = m.Namespace(
     "org.polarsys.capella.filtering",
     "7.0.0",
 )
-
-VIEWPOINT: t.Final = NS.viewpoint
-NAMESPACE: t.Final = NS.uri.format(VERSION="6.0.0")
-SYMBOLIC_NAME: t.Final = NS.alias
 
 
 class FilteringModel(capellacore.NamedElement):
@@ -80,7 +77,9 @@ class FilteringResults(capellacore.NamedElement):
 
 
 class AbstractFilteringResult(capellacore.NamedElement, abstract=True):
-    pass
+    @property
+    @abc.abstractmethod
+    def filtered_objects(self) -> m.ElementList: ...
 
 
 class FilteringResult(FilteringCriterionSet, AbstractFilteringResult):
@@ -135,6 +134,22 @@ class ComposedFilteringResult(AbstractFilteringResult):
     exclusion = m.Containment["ExclusionFilteringResultSet"](
         "ExclusionFilteringResultSet", (NS, "ExclusionFilteringResultSet")
     )
+
+    @property
+    def filtered_objects(self) -> m.ElementList:
+        ids = set[str]()
+
+        for result in self.union.results:
+            ids |= {i.uuid for i in result}
+
+        for result in self.intersection.results:
+            ids &= {i.uuid for i in result}
+
+        for result in self.exclusion.results:
+            ids -= {i.uuid for i in result}
+
+        elems = [self._model._loader[i] for i in ids]
+        return m.ElementList(self._model, elems)
 
 
 class FilteringResultSet(capellacore.NamedElement):
