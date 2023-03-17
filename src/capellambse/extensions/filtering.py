57fc68a916e45df069fb01a841d69ee69bf6f4ac
Martin Lehmann <martin.lehmann@deutschebahn.com>
2023-03-17 16:49:47 +0100

    feat(model): Migrate the Filtering extension to the new metamodel

--- src/capellambse/extensions/filtering.py
+++ src/capellambse/extensions/filtering.py
@@ -12,13 +12,14 @@ import pathlib
 import shutil
 import sys
 import typing as t
+import warnings
 
 import typing_extensions as te
 
 import capellambse
-import capellambse.metamodel as mm
 import capellambse.model as m
 from capellambse import _native, helpers
+from capellambse.metamodel import capellacore, capellamodeller
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -34,9 +35,22 @@ NAMESPACE: t.Final = NS.uri.format(VERSION="6.0.0")
 SYMBOLIC_NAME: t.Final = NS.alias
 
 
-class FilteringCriterion(m.ModelElement):
-    """A single filtering criterion."""
+class FilteringModel(capellacore.NamedElement):
+    criteria = m.Containment["FilteringCriterion"](
+        "ownedFilteringCriteria", (NS, "FilteringCriterion")
+    )
+    criterion_pkgs = m.Containment["FilteringCriterionPkg"](
+        "ownedFilteringCriterionPkgs", (NS, "FilteringCriterionPkg")
+    )
+    variability_features = m.Containment["FilteringCriterion"](
+        "ownedVariabilityFeatures", (NS, "FilteringCriterion")
+    )
 
+    if not t.TYPE_CHECKING:
+        criterion_packages = m.DeprecatedAccessor("criterion_pkgs")
+
+
+class FilteringCriterion(capellacore.NamedElement):
     _xmltag = "ownedFilteringCriteria"
 
     filtered_objects = m.Backref[m.ModelElement](
@@ -44,29 +58,106 @@ class FilteringCriterion(m.ModelElement):
     )
 
 
-class FilteringCriterionPkg(m.ModelElement):
+class FilteringCriterionSet(capellacore.NamedElement, abstract=True):
+    filtering_criteria = m.Association["FilteringCriterion"](
+        (NS, "FilteringCriterion"), "filteringCriteria"
+    )
+    variability_features = m.Association["FilteringCriterion"](
+        (NS, "FilteringCriterion"), "variabilityFeatures"
+    )
+
+
+class FilteringResults(capellacore.NamedElement):
+    results = m.Containment["AbstractFilteringResult"](
+        "filteringResults", (NS, "AbstractFilteringResult")
+    )
+    result_pkgs = m.Containment["FilteringResultPkg"](
+        "ownedFilteringResultPkgs", (NS, "FilteringResultPkg")
+    )
+    configurations = m.Containment["FilteringResult"](
+        "configurations", (NS, "FilteringResult")
+    )
+
+
+class AbstractFilteringResult(capellacore.NamedElement, abstract=True):
+    pass
+
+
+class FilteringResult(FilteringCriterionSet, AbstractFilteringResult):
+    pass
+
+
+class AssociatedFilteringCriterionSet(
+    FilteringCriterionSet, emde.ElementExtension
+):
+    pass
+
+
+class CreationDefaultFilteringCriterionSet(
+    FilteringCriterionSet, emde.ElementExtension
+):
+    pass
+
+
+class FilteringResultPkg(capellacore.Namespace):
+    results = m.Containment["AbstractFilteringResult"](
+        "ownedFilteringResults", (NS, "AbstractFilteringResult")
+    )
+    packages = m.Containment["FilteringResultPkg"](
+        "ownedFilteringResultPkgs", (NS, "FilteringResultPkg")
+    )
+    result_pkgs = m.Alias["m.ElementList[FilteringResultPkg]"]("packages")
+
+
+class FilteringCriterionPkg(capellacore.Namespace):
     """A package containing multiple filtering criteria."""
 
     _xmltag = "ownedFilteringCriterionPkgs"
 
-    criteria = m.DirectProxyAccessor(FilteringCriterion, aslist=m.ElementList)
-    packages: m.Accessor[m.ElementList[FilteringCriterionPkg]]
-
-
-class FilteringModel(m.ModelElement):
-    """A filtering model containing criteria to filter by."""
-
-    criteria = m.DirectProxyAccessor(FilteringCriterion, aslist=m.ElementList)
-    criterion_packages = m.DirectProxyAccessor(
-        FilteringCriterionPkg, aslist=m.ElementList
+    criteria = m.Containment["FilteringCriterion"](
+        "ownedFilteringCriteria", (NS, "FilteringCriterion")
     )
+    packages = m.Containment["FilteringCriterionPkg"](
+        "ownedFilteringCriterionPkgs", (NS, "FilteringCriterionPkg")
+    )
+
+
+class ComposedFilteringResult(AbstractFilteringResult):
+    """A result obtained from boolean operations of other results."""
+
+    union = m.Containment["UnionFilteringResultSet"](
+        "UnionFilteringResultSet", (NS, "UnionFilteringResultSet")
+    )
+    intersection = m.Containment["IntersectionFilteringResultSet"](
+        "IntersectionFilteringResultSet",
+        (NS, "IntersectionFilteringResultSet"),
+    )
+    exclusion = m.Containment["ExclusionFilteringResultSet"](
+        "ExclusionFilteringResultSet", (NS, "ExclusionFilteringResultSet")
+    )
+
+
+class FilteringResultSet(capellacore.NamedElement):
+    results = m.Association["AbstractFilteringResult"](
+        (NS, "AbstractFilteringResult"), "filteringResults"
+    )
+
+
+class UnionFilteringResultSet(FilteringResultSet):
+    pass
+
+
+class ExclusionFilteringResultSet(FilteringResultSet):
+    pass
+
+
+class IntersectionFilteringResultSet(FilteringResultSet):
+    pass
 
 
 class AssociatedCriteriaAccessor(m.PhysicalAccessor[FilteringCriterion]):
     def __init__(self) -> None:
-        super().__init__(
-            FilteringCriterion, aslist=m.ElementList[FilteringCriterion]
-        )
+        super().__init__(FilteringCriterion, aslist=m.ElementList)
 
     @t.overload
     def __get__(self, obj: None, objtype: type[t.Any]) -> te.Self: ...
@@ -96,27 +187,15 @@ class AssociatedCriteriaAccessor(m.PhysicalAccessor[FilteringCriterion]):
         return self._make_list(obj, elems)
 
 
-class FilteringResult(m.ModelElement):
-    """A filtering result."""
-
-
-class ComposedFilteringResult(m.ModelElement):
-    """A composed filtering result."""
-
-
 def init() -> None:
-    mm.capellamodeller.SystemEngineering.filtering_model = (
-        m.DirectProxyAccessor(FilteringModel)
+    capellamodeller.SystemEngineering.filtering_model = m.DirectProxyAccessor(
+        FilteringModel
     )
     m.MelodyModel.filtering_model = property(  # type: ignore[attr-defined]
         operator.attrgetter("project.model_root.filtering_model")
     )
     m.ModelElement.filtering_criteria = AssociatedCriteriaAccessor()
 
-
-FilteringCriterionPkg.packages = m.DirectProxyAccessor(
-    FilteringCriterionPkg, aslist=m.ElementList
-)
 
 try:
     import click
@@ -271,3 +350,20 @@ else:
 
 if __name__ == "__main__":
     _main()
+
+
+if not t.TYPE_CHECKING:
+
+    def __getattr__(name):
+        renames = {
+            "AbstractResultSet": FilteringResultSet,
+            "FilteringResult": AbstractFilteringResult,
+        }
+        if renamed := renames.get(name):
+            warnings.warn(
+                f"{name} has been renamed to {renamed.__name__}",
+                DeprecationWarning,
+                stacklevel=2,
+            )
+            return renamed
+        raise KeyError(name)
